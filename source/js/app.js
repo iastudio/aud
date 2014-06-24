ad6ad3a5ff53a3559fc338490aee8ca118de60b9
Ivan Rudoy - Work <rudooy@gmail.com>
2014-06-25 00:16:42 +1100

    fixes (IE, else)

--- source/js/app.js
+++ source/js/app.js
@@ -12,34 +12,41 @@
  //   MAP   //
  /////////////
 
+(function(){
 
-ymaps.ready(function () {
- var myMap = new ymaps.Map('map', {
-     center: [43.110067, 131.874974],
-     zoom: 17,
-     offset: [100, 100],
-     controls: []
- });
+    if (document.getElementById('map')) {
 
- var myPlacemark = new ymaps.Placemark([43.110067, 131.874974], {
-     balloonContentBody: [
-         '<address>',
-         '<strong>Профессиональный аудиторский центр</strong>',
-         '<br/>',
-         'Адрес: г.Владивосток, ул.Станюковича, д.3, к.2 «Сердце океана»',
-         '<br/>',
-         'Тел/факс: 8 (423) 279-02-18, 279-02-19, 260-72-88, 260-72-89',
-         '</address>'
-     ].join('')
- }, {
-     preset: 'islands#dotIcon',
-     iconColor: '#1CB083'
+        ymaps.ready(function () {
+         var myMap = new ymaps.Map('map', {
+             center: [43.110067, 131.874974],
+             zoom: 17,
+             offset: [100, 100],
+             controls: []
+         });
 
- });
+         var myPlacemark = new ymaps.Placemark([43.110067, 131.874974], {
+             balloonContentBody: [
+                 '<address>',
+                 '<strong>Профессиональный аудиторский центр</strong>',
+                 '<br/>',
+                 'Адрес: г.Владивосток, ул.Станюковича, д.3, к.2 «Сердце океана»',
+                 '<br/>',
+                 'Тел/факс: 8 (423) 279-02-18, 279-02-19, 260-72-88, 260-72-89',
+                 '</address>'
+             ].join('')
+         }, {
+             preset: 'islands#dotIcon',
+             iconColor: '#1CB083'
 
- myMap.geoObjects.add(myPlacemark);
- myMap.behaviors.disable('scrollZoom');
-});
+         });
+
+         myMap.geoObjects.add(myPlacemark);
+         myMap.behaviors.disable('scrollZoom');
+        });
+
+    }
+
+})();
 
 
 ////////////////////////
